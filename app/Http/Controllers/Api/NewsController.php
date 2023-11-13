d3fcb1b7ed19149ad89602224b1f66f25f3ff787
Rhoebie <rmviruse@gmail.com>
2023-11-13 17:11:15 +0800

    Update NewsController.php

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -10,7 +10,6 @@ class NewsController extends Controller
     public function getNews()
     {
         $feedUrl = 'https://www.manilatimes.net/news/feed/';
-        $maxItems = 20;
 
         $feed = new SimplePie();
         $feed->set_feed_url($feedUrl);
@@ -19,16 +18,14 @@ class NewsController extends Controller
 
         $news = [];
 
-        foreach ($feed->get_items(0, $maxItems) as $item) {
+        foreach ($feed->get_items() as $item) {
             $news[] = [
                 'title' => $item->get_title(),
                 'author' => $item->get_author()->get_name(),
-                // Get the author's name
                 'link' => $item->get_permalink(),
                 'description' => $item->get_description(),
                 'date' => $item->get_date('Y-m-d H:i:s'),
                 'image' => $item->get_enclosure()->get_link(),
-                // Get the URL of the image or thumbnail
             ];
         }
 
