0e7bc2598a1964ff8a475aab40a3edf8773769b0
Rhoebie <rmviruse@gmail.com>
2023-11-14 08:01:25 +0800

    Update NewsController.php

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -2,23 +2,34 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use SimplePie;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Pagination\Paginator;
 
 class NewsController extends Controller
 {
-    public function getNews()
+    public function getNews(Request $request)
     {
         $feedUrl = 'https://www.manilatimes.net/news/feed/';
 
         $feed = new SimplePie();
         $feed->set_feed_url($feedUrl);
-        $feed->enable_cache(false); // Disable caching for simplicity
+        $feed->enable_cache(false);
         $feed->init();
+
+        $perPage = $request->input('per_page', 10);
+        $currentPage = $request->input('page', 1);
+
+        // Get the total number of items
+        $totalItems = count($feed->get_items());
+
+        // Use Laravel's Paginator to paginate the array
+        $items = array_slice($feed->get_items(), ($currentPage - 1) * $perPage, $perPage);
 
         $news = [];
 
-        foreach ($feed->get_items() as $item) {
+        foreach ($items as $item) {
             $news[] = [
                 'title' => $item->get_title(),
                 'author' => $item->get_author()->get_name(),
@@ -29,6 +40,19 @@ class NewsController extends Controller
             ];
         }
 
-        return response()->json($news);
+        // Create a paginator instance
+        $paginator = new Paginator($news, $perPage, $currentPage);
+
+        // Add additional information to the paginator response
+        $paginator->appends('per_page', $perPage);
+
+        return response()->json([
+            'data' => $paginator->items(),
+            'meta' => [
+                'total' => $totalItems,
+                'per_page' => $perPage,
+                'current_page' => $currentPage,
+            ],
+        ]);
     }
 }
